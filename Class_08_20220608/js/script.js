b6c7046974cf0cfdd1ae8a9844b4905b69f4563d
Davis <daavis1995@gmail.com>
2022-06-08 19:22:26 +0300

    js

---
+++ Class_08_20220608/js/script.js
@@ -0,0 +1,10 @@
+a = 45;
+
+console.log(a);
+
+<script>
+let carName = "Volvo";
+document.getElementById("demo").innerHTML = carName;
+</script>
+
+// document.getElementById("demo").innerHTML = 456 || 0;
