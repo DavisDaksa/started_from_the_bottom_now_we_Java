80ee0bc348322dc94cd6980c76146f8cf79ceb65
Davis <daavis1995@gmail.com>
2022-06-13 18:37:54 +0300

    dectobin

--- Class_08_20220608/js/script.js
+++ Class_08_20220608/js/script.js
@@ -1,10 +1,45 @@
+var x = 45;
+document.getElementById("dec").innerHTML = x;
+
+
+// string to binary
+var y = (x << 0) >> 7;
+var z = String(y)
+y = (x << 1) >> 7;
+z = z+String(y);
+
+
+var y = (x << 0) >> 7;
+var z = String(y);
+y = (x << 1) >> 7;
+z = z + String(y);
+y = (x << 2) >> 7;
+z = z + String(y);
+y = (x << 3) >> 7;
+z = z + String(y);
+y = (x << 4) >> 7;
+z = z + String(y);
+y = (x << 5) >> 7;
+z = z + String(y);
+y = (x << 6) >> 7;
+z = z + String(y);
+y = (x << 7) >> 7;
+z = z + String(y);
+
+var bit_number = 7^1048;
+// y = '0b00101101'
+// var pielietot standarta js funkciju, kas pārveido dec to bin
+document.getElementById("bin").innerHTML = z;
 a = 45;
 
 console.log(a);
 
-<script>
-let carName = "Volvo";
-document.getElementById("demo").innerHTML = carName;
-</script>
+
+// let carName = "Volvo";
+//document.getElementById("demo").innerHTML = carName;
+// <!-- scope, koda bloks, ko nodala ar {}-->
+
+//var a=45;
+// console.log("a vērtība (no 'galvenā zara' pirms scope")
 
 // document.getElementById("demo").innerHTML = 456 || 0;
---
+++ Class_09_20221306/dectobin.js
@@ -0,0 +1,31 @@
+var x = 60;
+document.getElementById("dec").innerHTML = X;
+
+/* >> un & interesējošo bitu novietot 0.pozīcijā un ar ((x>>?) &1)
+*/
+var bit bit_sequence = " ";
+var zero_th_bit = (x >> 0) & 1;
+
+/* 
+x>> 0 -> 60(dec) atbilst 0011 1100(bin)
+0011 11_0_0 >> 0 -> 0011 1100
+               1(dec) tbilst 0000 0001(bin)
+               (x >> 0) & 1 -> 0011 1100
+                 0000 0001
+                 0000 0000(bin) - > 0(dec)
+                 */
+ console.log("0.bita attēlošana(pēc nobīdes pa 0 bitiem pa labi un 'reizināšanas' ar 1)+ zero_th_bit);
+ bit_sequence = zero_th_bit + bit_sequence;
+ console.log(bit_sequence)               
+
+ zero_th_bit = ( x>>1) & 1;
+
+ /* 
+ 0011 11_0_0 >> 1 -> 0001 1110
+(x >>1) & 1 -> 0001 1110
+               0001 
+ 
+ */ 
+console.log("1.bita attēlošana(pēc nobīdes pa 0 bitiem pa labi un 'reizināšanas' ar 1)+ zero_th_bit);
+ bit_sequence = zero_th_bit + bit_sequence;
+ console.log(bit_sequence)   
